a0110b8d8a407654e5a2dd1614de7f506627035d
Samantha Grothaus <70555340+sgrothaus97@users.noreply.github.com>
2020-11-09 21:50:19 -0800

    changed some wording in main.html and fixed random quote issue

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $("#searchBtn").on("click", function(event){
     $("#gifs-go-here").empty();  
     $("#definition").empty();
     $("#quotes-go-here").empty();
-  //search for gif of animal searched for from Giphy API.
+  //search for gif of word searched for from Giphy API.
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
   searchWord + "&api_key=dc6zaTOxFJmzC&limit=10";
   $.ajax({
@@ -33,7 +33,7 @@ $("#searchBtn").on("click", function(event){
       })
   
     var dictionaryURL = "https://dictionaryapi.com/api/v3/references/learners/json/"+ searchWord + "?key=ef3cb261-7a7c-465a-b6f5-4115280961f7"
-  //Merriam Webster API to pull a short learners definition of animal from search
+  //Merriam Webster API to pull a short learners definition of word from search
   $.ajax({
       url: dictionaryURL,
       method: "GET"
@@ -48,8 +48,8 @@ $("#searchBtn").on("click", function(event){
           $("#definition").append(defText)
           
       })
-      //Ajax to pull a random quote with searched animal in it
-      var quoteURL = "https://quote-garden.herokuapp.com/api/v2/quotes/"+ searchWord +"?"
+      //Ajax to pull a random quote with the searched word in it
+      var quoteURL = "https://quote-garden.herokuapp.com/api/v2/quotes/" + " " + searchWord + " ?"
       $.ajax({
         url: quoteURL,
         method: "GET"
@@ -57,9 +57,9 @@ $("#searchBtn").on("click", function(event){
         .then(function(response) {
             console.log(response)
             var quotes = response.quotes[0]
-            var quoteTitle = $(`<h2>${"Example of "+ searchWord}</h2>`)
+            var quoteTitle = $(`<h3>${"Random Quote"}</h3>`)
 
-            var quote = $(`<p>${quotes.quoteText}<br>${"By: "+ quotes.quoteAuthor}</p>`)
+            var quote = $(`<p>${quotes.quoteText}<br>${"~ "+ quotes.quoteAuthor}</p>`)
             $("#quotes-go-here").append(quoteTitle)
             $("#quotes-go-here").append(quote)
   
