8d1eb6476c340f89ff38627afd732f2c432dcc69
Rebecca Blanton <beccaablanton@gmail.com>
2020-11-09 21:03:09 -0800

    added search history buttons to page

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+var sideWords =["Unicorn", "Platypus","Water", "Rainbows"]
+var savedSearch = localStorage.getItem("searched word")
+//when searching pops up three ajax calls of gif, definition and a random quote
 $("#searchBtn").on("click", function(event){
+    event.preventDefault();
+  var searchWord = $("#search-animal").val().trim()
+  sideWords.push(searchWord)
+  localStorage.setItem("searched word", searchWord)
+  runSearchBar(searchWord);
+  renderSearchHistory();
+})
+
+  function runSearchBar(searchWord){
     $("#gifs-go-here").empty();  
     $("#definition").empty();
     $("#quotes-go-here").empty();
-    event.preventDefault();
-  var animal = $("#search-animal").val().trim()
   //search for gif of animal searched for from Giphy API.
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+  searchWord + "&api_key=dc6zaTOxFJmzC&limit=10";
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -16,13 +26,13 @@ $("#searchBtn").on("click", function(event){
       var results = response.data[0];
       var gifDiv = $("<div>");
           // Creating an image tag
-          var animalImage = $("<img>");
-          animalImage.attr("src", results.images.fixed_height.url);
-          gifDiv.append(animalImage);
+          var gifImage = $("<img>");
+          gifImage.attr("src", results.images.fixed_height.url);
+          gifDiv.append(gifImage);
           $("#gifs-go-here").prepend(gifDiv);
       })
   
-      var dictionaryURL = "https://dictionaryapi.com/api/v3/references/learners/json/"+ animal + "?key=ef3cb261-7a7c-465a-b6f5-4115280961f7"
+    var dictionaryURL = "https://dictionaryapi.com/api/v3/references/learners/json/"+ searchWord + "?key=ef3cb261-7a7c-465a-b6f5-4115280961f7"
   //Merriam Webster API to pull a short learners definition of animal from search
   $.ajax({
       url: dictionaryURL,
@@ -32,14 +42,14 @@ $("#searchBtn").on("click", function(event){
           console.log(response)
           var title = response[0].meta.id
           var definition = response[0].shortdef[0]
-          var defTitle = $(`<h1>${title}</h1>`)
+          var defTitle = $(`<h2>${title}</h2><br><h3>Definition</h3>`)
           var defText = $(`<p>${definition}</p>`)
           $("#definition").append(defTitle)
           $("#definition").append(defText)
           
       })
       //Ajax to pull a random quote with searched animal in it
-      var quoteURL = "https://quote-garden.herokuapp.com/api/v2/quotes/"+ animal +"?"
+      var quoteURL = "https://quote-garden.herokuapp.com/api/v2/quotes/"+ searchWord +"?"
       $.ajax({
         url: quoteURL,
         method: "GET"
@@ -47,7 +57,7 @@ $("#searchBtn").on("click", function(event){
         .then(function(response) {
             console.log(response)
             var quotes = response.quotes[0]
-            var quoteTitle = $(`<h2>${"Random quote about "+ animal +"'s:"}</h2>`)
+            var quoteTitle = $(`<h2>${"Example of "+ searchWord}</h2>`)
             var quote = $(`<p>${quotes.quoteText}<br>${"By: "+ quotes.quoteAuthor}</p>`)
             $("#quotes-go-here").append(quoteTitle)
             $("#quotes-go-here").append(quote)
@@ -55,4 +65,26 @@ $("#searchBtn").on("click", function(event){
             
         })
   
-    });
+    }
+
+    //search history
+   
+    //when searches, pulls that input from search and generates info on the city
+
+//builds search history
+function renderSearchHistory(){
+  $("#searchHistory").empty();
+  for(var i=0; i <sideWords.length; i++){
+  var pastSearch = $("<button>");
+  pastSearch.addClass("searchedWord")
+  pastSearch.attr("data-name", sideWords[i]);
+  pastSearch.text(sideWords[i]);
+  $("#searchHistory").prepend(pastSearch)
+
+  } }
+  //if previous searched city is clicked, pulls up data on that city
+$(document).on("click", ".searchedWord", function(){
+  runSearchBar($(this).attr("data-name"));
+} )
+
+renderSearchHistory();
