93b5c67eb2b492d36952c139368dbaeeee273936
Rebecca Blanton <beccaablanton@gmail.com>
2020-11-09 16:03:43 -0800

    added Ajax call for quote API to add  random quote with the animal searched in the quote

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $("#searchBtn").on("click", function(event){
     $("#gifs-go-here").empty();  
     $("#definition").empty();
+    $("quotes-go-here").empty();
     event.preventDefault();
   var animal = $("#search-animal").val().trim()
   //search for gif of animal searched for from Giphy API.
@@ -31,11 +32,27 @@ $("#searchBtn").on("click", function(event){
           console.log(response)
           var title = response[0].meta.id
           var definition = response[0].shortdef[0]
-          var defTitle = $(`<h2>${title}</h2>`)
+          var defTitle = $(`<h1>${title}</h1>`)
           var defText = $(`<p>${definition}</p>`)
           $("#definition").append(defTitle)
           $("#definition").append(defText)
           
       })
+      //Ajax to pull a random quote with searched animal in it
+      var quoteURL = "https://quote-garden.herokuapp.com/api/v2/quotes/"+ animal +"?"
+      $.ajax({
+        url: quoteURL,
+        method: "GET"
+      })
+        .then(function(response) {
+            console.log(response)
+            var quotes = response.quotes[0]
+            var quoteTitle = $(`<h2>${"Random quote about "+ animal +"'s:"}</h2>`)
+            var quote = $(`<p>${quotes.quoteText}<br>${"By: "+ quotes.quotesAuthor}</p>`)
+            $("#quotes-go-here").append(quoteTitle)
+            $("#quotes-go-here").append(quote)
+  
+            
+        })
   
     });
